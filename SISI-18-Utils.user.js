caedb73b8cf398631aeae4d01c3a4c878814dcb1
Etiendeval <44159397+Etiendeval@users.noreply.github.com>
2019-03-31 17:30:28 +0200

    1er essait
    1er essait

--- SISI-18-Utils.user.js
+++ SISI-18-Utils.user.js
@@ -2,7 +2,7 @@
 
 // @name			    SISI-18-Utils (Alpha)
 // @namespace     https://github.com/Etiendeval/SISI-18-Utils
-// @version			  1.7
+// @version			  1.8.1
 // @author        Etiendeval
 // @description 	Script for play GoodGameEmpire in fullscreen for spilgames.com (Alpha)
 // @updateURL     https://github.com/Etiendeval/SISI-18-Utils/raw/master/SISI-18-Utils.user.js
@@ -36,6 +36,29 @@
 // @match			    http://www.spelletjes.nl/goodgames/sns/init/*
 // @match			    http://www.spelletjes.nl/spel/goodgame-empire.html
 
+// @match			    https://www.jeu.fr/goodgames/sns/init/*
+// @match			    https://www.jeu.fr/jeu/Goodgame-Empire.html
+// @match			    https://www.jeux.fr/goodgames/sns/init/*
+// @match			    https://www.jeux.fr/jeu/Goodgame-Empire.html
+// @match			    https://www.gioco.it/goodgames/sns/init/*
+// @match			    https://www.gioco.it/gioco/goodgame-empire.html
+// @match			    https://www.spelletjes.nl/goodgames/sns/init/*
+// @match			    https://www.spelletjes.nl/spel/goodgame-empire.html
+// @match			    https://www.giochi.it/goodgames/sns/init/*
+// @match			    https://www.giochi.it/gioco/goodgame-empire.html
+// @match			    https://www.jetztspielen.de/goodgames/sns/init/*
+// @match			    https://www.jetztspielen.de/spiel/goodgame-empire.html
+// @match			    https://www.gry.pl/goodgames/sns/init/*
+// @match			    https://www.gry.pl/gra/goodgame-empire.html
+// @match			    https://www.spel.nl/goodgames/sns/init/*
+// @match			    https://www.spel.nl/spel/goodgame-empire.html
+// @match			    https://www.spielen.com/goodgames/sns/init/*
+// @match			    https://www.spielen.com/spiel/goodgame-empire.html
+// @match			    https://www.spela.se/goodgames/sns/init/*
+// @match			    https://www.spela.se/spel_/goodgame-empire.html
+// @match			    https://www.spelletjes.nl/goodgames/sns/init/*
+// @match			    https://www.spelletjes.nl/spel/goodgame-empire.html
+
 // @match			    http://www.mousebreaker.com/goodgames/sns/init/*
 // @match			    http://www.mousebreaker.com/game/goodgame-empire?
 // @match			    http://www.juegos.com/goodgames/sns/init/*
@@ -45,75 +68,35 @@
 // @match			    http://www.games.co.uk/goodgames/sns/init/*
 // @match			    http://www.games.co.uk/game/goodgame-empire.html
 
+// @match			    https://www.mousebreaker.com/goodgames/sns/init/*
+// @match			    https://www.mousebreaker.com/game/goodgame-empire?
+// @match			    https://www.juegos.com/goodgames/sns/init/*
+// @match			    https://www.juegos.com/juego/goodgame-empire?
+// @match			    https://www.agame.com/goodgames/sns/init/*
+// @match			    https://www.agame.com/game/goodgame-empire.html
+// @match			    https://www.games.co.uk/goodgames/sns/init/*
+// @match			    https://www.games.co.uk/game/goodgame-empire.html
+
 // ==/UserScript==
+console.log('[UserScript] Preboot');
+
+//-->Pour une future option de mise en plaine écrant automatique ?
 var auto_run = GM_getValue('auto-run');
 if(auto_run == undefined){GM_setValue('auto-run','false');
   auto_run = GM_getValue('auto-run');
-}
-GM_log('auto_run_info '+auto_run);
+};
+//<--
+GM_log('[UserScript] auto_run_info '+auto_run);
 
+console.log('[UserScript] Boot');
 $(document).ready(function() {
 
-var chaine='<html style="height:100%;">'+
+var HTML='<html style="height:100%;">'+
 ''+
 '<head>'+
 '  <title>NOMJOUEUR ( SISI 18 Utils )</title>'+
 '  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>'+
 '  <meta name="viewport" content="width=device-width, initial-scale=1">'+
-'  <style>'+
-'  .switch {'+
-'    position: relative;'+
-'    top: -9px;'+
-'    display: inline-block;'+
-'    width: 30px;'+
-'    height: 17px;'+
-'  }'+
-'  .switch input {'+
-'    opacity: 0;'+
-'    width: 0;'+
-'    height: 0;'+
-'  }'+
-'  .slider {'+
-'    position: absolute;'+
-'    cursor: pointer;'+
-'    top: 0;'+
-'    left: 0;'+
-'    right: 0;'+
-'    bottom: 0;'+
-'    background-color: #ccc;'+
-'    -webkit-transition: .3s;'+
-'    transition: .3s;'+
-'  }'+
-'  .slider:before {'+
-'    position: absolute;'+
-'    content: "";'+
-'    height: 13px;'+
-'    width: 13px;'+
-'    left: 2px;'+
-'    bottom: 2px;'+
-'    background-color: white;'+
-'    -webkit-transition: .3s;'+
-'    transition: .3s;'+
-'  }'+
-'  input:checked + .slider {'+
-'    background-color: #2196F3;'+
-'  }'+
-'  input:focus + .slider {'+
-'    box-shadow: 0 0 1px #2196F3;'+
-'  }'+
-'  input:checked + .slider:before {'+
-'    -webkit-transform: translateX(13px);'+
-'    -ms-transform: translateX(13px);'+
-'    transform: translateX(13px);'+
-'  }'+
-'/* Rounded sliders */'+
-'  .slider.round {'+
-'    border-radius: 13px;'+
-'  }'+
-'  .slider.round:before {'+
-'    border-radius: 50%;'+
-'  }'+
-'  </style>'+
 '</head>'+
 ''+
 '<body style="margin:0px;padding:0px;">'+
@@ -122,8 +105,7 @@ var chaine='<html style="height:100%;">'+
 '    function Rel() {'+
 '      document.getElementById(\'flashcontent\').parentNode.innerHTML=document.getElementById(\'flashcontent\').parentNode.innerHTML;'+
 '    }'+
-'    var auto_run_info;'+
-'    auto_run_info='+ auto_run +';'+
+'    var auto_run_info =auto_run;'+
 '    var memo;'+
 '    memo=\'\';'+
 ''+
@@ -166,20 +148,6 @@ var chaine='<html style="height:100%;">'+
 '      dime2=dime-2;'+
 '      $(\'#flashcontent\').css("height",dime2+\'%\');'+
 '    }'+
-'    function check(){'+
-'      $("#auto_run").prop("checked", true);'+
-'    }'+
-'    function uncheck(){'+
-'      $("#auto_run").prop("checked", false);'+
-'    }'+
-'    function MAJ_checkbox(){'+
-'      if(auto_run_info===true){'+
-'        check();'+
-'      }'+
-'      if(auto_run_info===false){'+
-'        uncheck();'+
-'      }'+
-'    }'+
 '  </script>'+
 ''+
 '  <div style=\'position:absolute;top:10px;left:10px;\'>'+
@@ -200,10 +168,6 @@ var chaine='<html style="height:100%;">'+
 '    <input type=\'button\' value=\'1/2\' onclick=\'ch_taille(50);\'>'+
 '    <input type=\'button\' value=\'3/4\' onclick=\'ch_taille(70);\'><br>'+
 '    Mise en pleine écrant automatiquement (alpha):'+
-'    <label class=\'switch\'>'+
-'      <input type=\'checkbox\'id=\'auto_run\'>'+
-'      <span class=\'slider round\'></span>'+
-'    </label>'+
 '  </div>'+
 '  <div id=\'gge\' style=\'float:left;width:100%;border:0px solid;\'>'+
 '  MOMOMOMO'+
@@ -211,19 +175,21 @@ var chaine='<html style="height:100%;">'+
 '  </body>'+
 '</html>';
 
+console.log('[UserScript] Postboot');
 var loc = window.location.href;
+var GUI_auto_run;
 
 if (loc.indexOf('http://gi.goodgamestudios.com/sns')!=-1 || loc.indexOf('jeux.fr/goodgames/sns/init/') != -1 || loc.indexOf('goodgames/sns/init/') != -1 ) {
     var count=0;
    var tttt=setInterval(function(e) {
 
-   ifr=document.getElementById("content-iframe");
+   ifr=document.getElementById("CastleUI");
        if(ifr!=null)
    button = ifr.contentDocument.getElementById('icon');
 	   if(button==null)  return;
        button.innerHTML='<input type="button" value="Full Window + Tools"/>';
     button.addEventListener('click',function() {
-        var str=ifr.contentDocument.getElementById('flashcontent');
+        var str=ifr.contentDocument.getElementById('CastleUI');
         var reg= new RegExp('MOMOMOMO',"g");
         var reg1= new RegExp('="800"',"g");
         var reg2= new RegExp('="600"',"g");
@@ -232,30 +198,11 @@ if (loc.indexOf('http://gi.goodgamestudios.com/sns')!=-1 || loc.indexOf('jeux.fr
         var nomjoueur=reg3.exec(str.innerHTML);
 
 		mwin=open('','_self');
-		mwin.parent.document.write(chaine.replace(reg4,nomjoueur[1]).replace(reg, str.parentNode.innerHTML.replace(reg1,'="100%"').replace(reg2,'="100%"')));
+		mwin.parent.document.write(HTML.replace(reg4,nomjoueur[1]).replace(reg, str.parentNode.innerHTML.replace(reg1,'="100%"').replace(reg2,'="100%"')));
+
     },true);
        clearInterval(tttt) ;
 
     }, 3000);
 }
-var tttt2=setInterval(function(e) {
-  pub=document.getElementById("sgAdWrapperObj");
-  if(pub!=null) {pub.parentNode.removeChild(pub);
-	   clearInterval(tttt2);
-  }
-pub=document.getElementById("sgAdLbGp728x90");
-  if(pub!=null) {pub.parentNode.removeChild(pub);
-  }
-pub=document.getElementById("sgAdLbSg728x90");
-	if(pub!=null) {pub.parentNode.removeChild(pub);
-  }
-pub=document.getElementById("cboxOverlay");
-	if(pub!=null) {pub.parentNode.removeChild(pub);
-	clearInterval(tttt2);
-	}
-pub=document.getElementById("colorbox");
-	if(pub!=null) {pub.parentNode.removeChild(pub);
-	clearInterval(tttt2);
-  }
-},1500);
 });
